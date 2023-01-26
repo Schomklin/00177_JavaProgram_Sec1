8a64522f837f2c11e51404dfec67d82f17d38eff
Schomklin <kungchomklin@gmail.com>
2023-01-26 09:49:47 +0700

    LabSheet10Example1

--- OOP_Concept/src/Student.java
+++ OOP_Concept/src/Student.java
@@ -1,37 +1,66 @@
 
 public class Student {
-	//attributes
+	// private instance variables
 	private String name;
-	private int score;
-	
-	//methods
-	public void setName(String Name) {
-		name = Name;		
+	private String address;
+	// The courses and grades for the courses are kept in 2 parallel arrays
+	// course[] = {INT108,INT107}; grades[]={89,75};
+	private String[] courses;
+	private int[] grades; // valid range is [0,100]
+	private int numCourses; // numbers of courses taken so far
+	// Maximum number of courses taken by student
+	private static final int MAX_COURSE = 30;
+
+	/** Constructs a Student instance with given input */
+	public Student(String name, String address) {
+		this.name = name;
+		this.address = address;
+		courses = new String[MAX_COURSE]; // allocate arrays
+		grades = new int[MAX_COURSE];
+		numCourses = 0; // no courses so far
 	}
-	
+
+	// The public getters and setters.
+	// No setter for name as it is not designed to be changed.
 	public String getName() {
-		return name;
+		return this.name;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String toString() {
+		return name + "(" + address + ")";
 	}
 	
-	public void setScore(int Score) {
-		score = Score;
+	/**Adds a course and grade*/
+	public void addCourseGrade(String course,int grade) {
+		courses[numCourses] = course;
+		grades[numCourses] = grade;
+		++numCourses;
 	}
 	
-	public int getScore() {
-		return score;
+	/**Prints all courses and their grades*/
+	public void printGrades() {
+		System.out.print(name); 
+		for(int i=0; i<numCourses;i++) {
+			System.out.print(" "+courses[i]+":"+grades[i]);			
+		}
+		System.out.println();
 	}
 	
-	public boolean checkScore() {
-		if(score>=0 && score<=100) 
-			return true;
-		else 
-			return false;			
+	/**Computes the average grade*/
+	public double getAverageGrade() {
+		int sum=0;
+		for(int i=0; i<numCourses;i++) {
+			sum += grades[i];
+		}
+		return (double)sum/numCourses;
 	}
-	
-	public boolean isPass() {
-		if(score>=50)
-			return true;
-		else 
-			return false;
-	}
+
 }
--- OOP_Concept/src/TestStudent.java
+++ OOP_Concept/src/TestStudent.java
@@ -1,32 +1,8 @@
-import java.util.*;
+
 public class TestStudent {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		Student[] std = new Student[3]; //Array Object
-		for(int i=0;i<std.length;i++) {
-			std[i] = new Student(); //กำหนดค่าของ array ให้เรียกใช้เมธอดในคลาสต้นแบบ			
-			System.out.println("INPUT INFORMATION OF STUDENT "+(i+1));
-			System.out.println("--------------------------------------");
-			System.out.print("Input student name: ");			
-			std[i].setName(scan.next());
-			System.out.print("Input student score:");
-			std[i].setScore(scan.nextInt());
-			while(!std[i].checkScore()) {
-				System.out.print("Input student score, again:");
-				std[i].setScore(scan.nextInt());
-			}//end while()
-			System.out.println();
-		} //end for()
-		System.out.println();
-		System.out.println("LIST OF PASS STUDENT (>=50)");
-		System.out.println("--------------------------------------");
-		for(int i=0;i<std.length;i++) {
-			if(std[i].isPass()) {
-				System.out.println(">> "+std[i].getName()+" "
-						+" ("+std[i].getScore()+" )");
-			}
-		}
 
 	}
 
